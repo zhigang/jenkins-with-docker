91d64f3456f451c1897759fbaf966091ad220d84
zhigang <zhigang@ZG-PC>
2025-06-09 16:35:00 +0800

    update shell

--- cmd/docker-install.sh
+++ cmd/docker-install.sh
@@ -14,8 +14,8 @@ chmod a+r /etc/apt/keyrings/docker.asc
 echo \
    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" |
-   sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-sudo apt-get update
+   tee /etc/apt/sources.list.d/docker.list >/dev/null
+apt-get update
 
 echo -e "\n 4.Install CS Docker Engine:"
 apt-get update &>/dev/null
