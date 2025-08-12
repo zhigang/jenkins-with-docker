35628abe68926f861f9dd01f21c8c9f49b3aab24
zhigang <zhigang52110@sina.com>
2025-08-12 20:20:10 +0800

    fix ca-certificates error

--- cmd/docker-install.sh
+++ cmd/docker-install.sh
@@ -6,6 +6,7 @@ set -o pipefail
 # Add Docker's official GPG key:
 apt-get update
 apt-get install -y ca-certificates curl
+update-ca-certificates
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
